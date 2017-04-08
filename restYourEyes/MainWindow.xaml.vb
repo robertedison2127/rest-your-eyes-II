e2ca077560bab83914b8d651eb2159d92a9be553
Robert Edison <robert@robertedison.co.uk>
2017-04-08 19:58:30 +0100

    Import of existing code

---
+++ restYourEyes/MainWindow.xaml.vb
@@ -0,0 +1,31 @@
+﻿
+Class MainWindow
+
+    Dim timeControl As New TimeController()
+
+    Private Sub btnStart_Click(sender As Object, e As RoutedEventArgs) Handles btnStart.Click
+        lblTimer.Content = "Timer is ON"
+        timeControl.startTimer(6000)
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As RoutedEventArgs) Handles btnStop.Click
+        lblTimer.Content = "Timer is OFF"
+        timeControl.stopTimer()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As RoutedEventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As RoutedEventArgs) Handles btnAbout.Click
+        Dim aboutWindow As New About
+        aboutWindow.ShowDialog()
+    End Sub
+
+    Public Sub updateLabel()
+        lblTimer.Content = "Timer is OFF"
+    End Sub
+
+
+    
+End Class
